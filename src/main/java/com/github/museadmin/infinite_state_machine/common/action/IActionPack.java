9aeead73f7380c7d3c58ef2a96040bac42f0d121
Bradley Atkins <bradley.atkins@bjss.com>
2018-07-14 15:22:56 +0100

    Trimmed back interface

--- src/main/java/com/github/museadmin/infinite_state_machine/common/action/IActionPack.java
+++ src/main/java/com/github/museadmin/infinite_state_machine/common/action/IActionPack.java
@@ -1,9 +1,7 @@
 package com.github.museadmin.infinite_state_machine.common.action;
 
 import org.json.JSONObject;
-import java.util.ArrayList;
 
 public interface IActionPack {
   JSONObject getJsonObjectFromResourceFile(String fileName);
-  ArrayList getActionsFromActionPack();
 }
