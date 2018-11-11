c924b70ec18576184c6906d025cce3f734023ad5
Bradley Atkins <Bradley.Atkins@bjss.com>
2018-11-11 12:46:12 +0000

    Extended scope of interface methods

--- src/main/java/com/github/museadmin/infinite_state_machine/common/action/IAction.java
+++ src/main/java/com/github/museadmin/infinite_state_machine/common/action/IAction.java
@@ -1,19 +1,34 @@
 package com.github.museadmin.infinite_state_machine.common.action;
 
 import com.github.museadmin.infinite_state_machine.common.dal.DataAccessLayer;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public interface IAction {
   void execute();
-  void activate(String actionName);
+
   Boolean active();
+  boolean active(String action);
+  void activate(String actionName);
   Boolean afterActionsComplete();
   Boolean beforeActionsComplete();
+  void clearPayload(String actionName);
+  String createRunDirectory(String directory);
+  void deactivate();
+  void deactivate(String actionFlag);
+  JSONObject getJsonObjectFromFile(String fileName);
+  ArrayList<JSONObject> getUnprocessedMessages();
+  void insertMessage(JSONObject message);
   void insertProperty(String property, String value);
+  void markMessageProcessed(Integer id);
   void setDataAccessLayer(DataAccessLayer dataAccessLayer);
   void setRunRoot(String runRoot);
   String queryProperty(String property);
   String queryRunPhase();
   void setState(String stateName);
+  void updatePayload(String actionName, String payload);
   void updateProperty(String property, String value);
+  void updateRunPhase(String runPhase);
   void unsetState(String stateName);
 }
